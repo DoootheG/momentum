2c2cce16a999ccc95195e2eba7a975436b42c355
Hank <dlwkdans@korea.ac.kr>
2022-08-12 17:28:07 +0900

    20220812 1727
    바닐라JS 연습...
    어렵구먼.......

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ const player = {
 console.log(player);
 player.tier = "platinum";
 console.log(player);
+
+function theg(a, b) {
+  console.log("내 책상에는 " + a + "개의 " + b + "이 있다.");
+}
+
+theg(1, "민트사탕");
+theg(4, "껌");
+theg(4, "책");
+theg(15, "볼펜");
+
+const greeting = {
+  firstMeet: function (a) {
+    console.log("Hello " + a + "! This is Hank. Nice to meet you!");
+  },
+};
+
+greeting.firstMeet("Lukas");
