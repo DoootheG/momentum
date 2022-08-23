c7e369485e3d788da3fa068594b3bd2069e57854
Hank <dlwkdans@korea.ac.kr>
2022-08-23 11:27:36 +0900

    20220823

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const h1 = document.querySelector(".hello h1");
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector(".greeting");
 
-function handleH1Click() {
-  h1.classList.toggle("active");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+function onLoginSumit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `안녕하세요! ${username}씨 :)`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-h1.addEventListener("click", handleH1Click);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSumit);
+} else {
+  greeting.innerText = `안녕하세요! ${savedUsername}씨 :)`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
